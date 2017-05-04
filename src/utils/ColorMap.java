a41af41cee022d06b46c0698e44f38ba56a5520a
takumus <me@takumus.com>
2017-05-04 18:12:57 +0900

    名前に変換できるように。

--- src/utils/ColorMap.java
+++ src/utils/ColorMap.java
@@ -11,6 +11,8 @@ import java.util.Map;
  */
 public class ColorMap {
     private static Map<DyeColor, ChatColor> dyeChatMap = new HashMap<>();
+    private static Map<DyeColor, String> dyeNameMap = new HashMap<>();
+    private static Map<String, DyeColor> nameDyeMap = new HashMap<>();
     static {
         dyeChatMap.put(DyeColor.BLACK, ChatColor.DARK_GRAY);
         dyeChatMap.put(DyeColor.BLUE, ChatColor.DARK_BLUE);
@@ -28,8 +30,35 @@ public class ColorMap {
         dyeChatMap.put(DyeColor.SILVER, ChatColor.GRAY);
         dyeChatMap.put(DyeColor.WHITE, ChatColor.WHITE);
         dyeChatMap.put(DyeColor.YELLOW, ChatColor.YELLOW);
+
+        dyeNameMap.put(DyeColor.BLACK, "Black");
+        dyeNameMap.put(DyeColor.BLUE, "Blue");
+        dyeNameMap.put(DyeColor.BROWN, "Brown");
+        dyeNameMap.put(DyeColor.CYAN, "Cyan");
+        dyeNameMap.put(DyeColor.GRAY, "Gray");
+        dyeNameMap.put(DyeColor.GREEN, "Green");
+        dyeNameMap.put(DyeColor.LIGHT_BLUE, "Light Blue");
+        dyeNameMap.put(DyeColor.LIME, "Lime");
+        dyeNameMap.put(DyeColor.MAGENTA, "Magenta");
+        dyeNameMap.put(DyeColor.ORANGE, "Orange");
+        dyeNameMap.put(DyeColor.PINK, "Pink");
+        dyeNameMap.put(DyeColor.PURPLE, "Purple");
+        dyeNameMap.put(DyeColor.RED, "Red");
+        dyeNameMap.put(DyeColor.SILVER, "Silver");
+        dyeNameMap.put(DyeColor.WHITE, "White");
+        dyeNameMap.put(DyeColor.YELLOW, "Yellow");
+
+        dyeNameMap.entrySet().forEach((set) -> {
+            nameDyeMap.put(set.getValue().toLowerCase(), set.getKey());
+        });
     }
     public static ChatColor getChatColor(DyeColor dyeColor) {
         return dyeChatMap.get(dyeColor);
+    }
+    public static String getName(DyeColor dyeColor) {
+        return dyeNameMap.get(dyeColor);
+    }
+    public static DyeColor getDyeColor(String name) {
+        return nameDyeMap.get(name);
     }
 }
