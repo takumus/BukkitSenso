11199c5a85be53104d8f09bd32b91c138efdf7b7
takumus <me@takumus.com>
2017-05-09 12:49:54 +0900

    タイマー作成

---
+++ src/timers/Timer.java
@@ -0,0 +1,13 @@
+package timers;
+
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by takumus on 2017/05/09.
+ */
+public class Timer {
+    public static void init(JavaPlugin plugin) {
+        Bukkit.getServer()
+    }
+}
